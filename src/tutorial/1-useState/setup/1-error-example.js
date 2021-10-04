17ad6b86a4dc9a1a92dd54cc4674a14061bb000b
sanmeshkakade <181210047@nitdelhi.ac.in>
2021-10-04 06:08:40 +0530

    04-10-21 update

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { Fragment } from "react";
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let title = "Random Title";
+  const handleClick = () => {
+    title = "New Title";
+    console.log("Clicked!");
+  };
+
+  return (
+    <React.Fragment>
+      <h2>{title}</h2>
+      <button type="button" className="btn" onClick={handleClick}>
+        Change Title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default ErrorExample;
